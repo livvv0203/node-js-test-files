93d2a14945d9cd3b2c6bfed57784918a069ad1ed
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2023-03-18 15:17:55 -0700

    first module use

---
+++ first-app.js
@@ -0,0 +1,12 @@
+// To enable the file system, first import to let node know we want to use the functionality
+// fs is one of node's core module, require is just an import syntax
+const fs = require('fs'); 
+// Write something in a file on Hard Drive
+fs.writeFileSync('hello.txt', 'Hello Hello!');
+
+
+
+
+
+
+
