f36ae6be88875102778c40adee48a5ab67484642
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2023-04-11 12:58:10 -0700

    Using express.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // Can also be named as Server.js
 // This is the root file
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
 
-// port, hostname, backlog
-server.listen(3003); // will keep listening on requests
+// Create an express application instance
+const app = express(); // Pass app as first argument to express constructor, for creating a server
+
+// next is a function that will be passed
+app.use((req, res, next) => {
+  console.log('In the Middleware!');
+  next(); // Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log('Finished in the Middleware!');
+    // ...
+    res.send('<h1>Hi from Jieqing!!!!</h1>'); // send response
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// // port, hostname, backlog
+// server.listen(3000); // will keep listening on requests
---
+++ test.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < 10; i++) {
+  for (var j = 1; j < 10; j++) {
+    console.log('J is: ' + j);
+  }
+  console.log('End ----> I is: ' + i);
+}
+
+
