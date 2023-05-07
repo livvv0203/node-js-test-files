d0215ebe6880ce15d3399cd469076fd5afe14f28
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2023-05-06 19:30:35 -0700

    In progress

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
-for (var i = 0; i < 10; i++) {
-  for (var j = 1; j < 10; j++) {
-    console.log('J is: ' + j);
+function TestFunction() {
+  for (var i = 0; i < 10; i++) {
+    for (var j = 1; j < 10; j++) {
+      console.log('J is: ' + j);
+    }
+    console.log('End ----> I is: ' + i);
   }
-  console.log('End ----> I is: ' + i);
 }
+
+function AnotherFunction(content) {
+  console.log('Calling Another Function: ' + content);
+}
+
+// TestFunction();
+// AnotherFunction('Test Test Test!!!');
+
+
+
+
 
 
