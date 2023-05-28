84043f28d14a03d37aa9d861807fac7956f0ff72
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-05-28 16:29:14 -0700

    doubly linked list -> push and pop methods

--- data-structure-basics/DoublyLinkList.js
+++ data-structure-basics/DoublyLinkList.js
@@ -12,12 +12,47 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // Push the node to the end of the list
+  push(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // remove a node at the end of the list
+  pop() {
+    if (!this.head) return undefined;
+
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      // update the tail
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
 
-let node = new Node(2);
-// console.log(node);
-
-let DLL = new DoublyLinkedList(5);
-// console.log(DLL);
-
-
+let DLL = new DoublyLinkedList();
+DLL.push(2);
+DLL.push(5);
+DLL.push(8);
+DLL.push("LAST ITEM");
+let poppedNode = DLL.pop();
+console.log(DLL);
+console.log("\n\nPopped Node is: \n\n");
+console.log(poppedNode);
