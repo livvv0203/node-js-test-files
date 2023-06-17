292553d9b7675db1493caf7abc0f13bcd8c95478
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-06-17 10:21:35 -0700

    Queue DS

---
+++ data-structure-basics/Queue.js
@@ -0,0 +1,47 @@
+// LIFO Structure
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  // return and remove the first element added in
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+var queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(7);
+
+console.log(queue.dequeue());
