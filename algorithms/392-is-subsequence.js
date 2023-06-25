d2439369abad1b8bad300062476a7290e78f9cdb
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-06-25 14:53:57 -0700

    Leetcode practice and tree ds

--- algorithms/392-is-subsequence.js
+++ algorithms/392-is-subsequence.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 
-var s = 'bbb';
-var t = 'ahbgdc';
+var s = "abc";
+var t = "ahbgdc";
 
 var isSubsequence = function (s, t) {
   if (s.length > t.length) return false;
@@ -20,4 +20,28 @@ var isSubsequence = function (s, t) {
   return subsequence === s.length;
 };
 
-console.log(isSubsequence(s, t));
+var isSubsequence_2 = function (s, t) {
+  // Edge case:
+  if (s.length > t.length) return false;
+
+  // loop char of shorter string
+  let foundSequence = [];
+  let sequence = 0;
+  for (let i = 0; i < s.length; i++) {
+    console.log("i is: " + s[i]);
+    for (let j = sequence; j < t.length; j++) {
+      if (s[i] !== t[j]) {
+        continue;
+      } else {
+        // found matching value
+        foundSequence.push(t[j]);
+        sequence = j + 1;
+        break;
+      }
+    }
+  }
+
+  return foundSequence.length === s.length;
+};
+
+console.log(isSubsequence_2(s, t));
--- data-structure-basics/Tree.js
+++ data-structure-basics/Tree.js
@@ -1,1 +1,6 @@
-// Common Data Structure
+// Common Non-linear Data Structure
+
+// Binary - at most 2 children
+
+// Binary Search Tree(BST) - Sorted in a way - all nodes to the left are less than the parent node
+
