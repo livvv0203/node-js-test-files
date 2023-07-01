3084249d5c9f7d1e0ebdec8fe1d9bd21a9d9be1e
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-07-01 12:03:03 -0700

    Tree Structure

--- data-structure-basics/Tree.js
+++ data-structure-basics/Tree.js
@@ -4,3 +4,24 @@
 
 // Binary Search Tree(BST) - Sorted in a way - all nodes to the left are less than the parent node
 
+// Node class - represents each node of the tree
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// BST class
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+
+
+console.log(tree);
