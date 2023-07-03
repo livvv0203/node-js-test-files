19b2cfbf77f1d375b26fff460e692451d43dcc85
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-07-03 10:45:51 -0700

    DFS - Implementation

--- data-structure-basics/Tree.js
+++ data-structure-basics/Tree.js
@@ -20,6 +20,7 @@ class BinarySearchTree {
   }
 
   // inserting a node - iteratively
+  // Time Complexity: O(Log n)
   insert(value) {
     var newNode = new Node(value);
     if (this.root === null) {
@@ -50,6 +51,52 @@ class BinarySearchTree {
   find(value) {
     // Starting at the root
     if (this.root === null) return false;
+
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        // Found the node
+        return true;
+      }
+    }
+    return false;
+  } // end of find method
+
+  // Breadth First Search
+  BFS() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+
+    queue.push(node);
+    // Check if something in the queue
+    while (queue.length) {
+      // take out one node from the beginning
+      node = queue.shift();
+      // push it into the list to be return
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  // DFS - PreOrder - Recursively
+  DFSPreOrder() {
+    let data = [];
+
+    // This is a helper function
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
   }
 }
 
@@ -58,7 +105,10 @@ class BinarySearchTree {
 //  2     7   11    16
 
 var tree = new BinarySearchTree();
-// tree.insert(10);
-// tree.insert(2);
-console.log(tree);
-console.log(tree.find(100));
+tree.insert(10);
+tree.insert(2);
+tree.insert(3);
+tree.insert(8);
+tree.insert(5);
+
+console.log(tree.DFSPreOrder());
