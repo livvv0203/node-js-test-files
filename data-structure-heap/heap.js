195378289f9401be7fe0eadbcea42d123c40a353
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-07-04 17:36:54 -0700

    remove and sink down in a binary heap

--- data-structure-heap/heap.js
+++ data-structure-heap/heap.js
@@ -42,12 +42,69 @@ class MaxBinaryHeap {
   /**
    * Removing from a Heap - ExtractMax
    */
-  
+  extractMax() {
+    // Swap the first value with the last one
+    // Store the Max value
+    const max = this.values[0];
+    // Pop it off
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      // Sink down or bubble down the node
+      this.sinkDown();
+    }
+
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0; // Sinking down from the root
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChildValue, rightChildValue;
+      let swap = null;
+
+      // check if left child index is inbound, to validate the index
+      if (leftChildIndex < length) {
+        leftChildValue = this.values[leftChildIndex];
+        if (leftChildValue > element) {
+          // keeps track of the postion that we are going to swap with
+          swap = leftChildIndex;
+        }
+      }
+
+      // check the same on right child index, if it's inbound or not
+      if (rightChildIndex < length) {
+        rightChildValue = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChildValue > element) ||
+          (swap !== null && rightChildValue > leftChildValue)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      // never swap, break out of the loop
+      if (swap === null) break;
+      // Do the swap
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+
+      // update new parent
+      index = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
 console.log(JSON.stringify(heap));
 heap.insert(55);
+console.log(heap.extractMax());
+console.log(heap.extractMax());
 console.log(JSON.stringify(heap));
 
 // [41, 39, 33, 18, 27, 12, 55]
