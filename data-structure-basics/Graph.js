f241087f92456d657ca3eec9b0a3ef0d42567766
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2023-07-15 01:10:09 -0700

    Add vertex and edge to a graph, remove edge functions.

--- data-structure-basics/Graph.js
+++ data-structure-basics/Graph.js
@@ -12,5 +12,40 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
-  
+
+  // Add Vertex Implementation
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  // Add relationships between two vertices - Edge
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    // keep everything that is not vertex 2
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+
+console.log(graph);
+graph.removeEdge("A", "B");
+
+console.log(graph);
