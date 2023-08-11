e20d21bfecdd8df01b4fae822c124513582e0aeb
ext.olivia.liu1 <ext.olivia.liu1@jd.com>
2023-08-11 15:10:01 -0700

    majority-ele

---
+++ algorithms/169-majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * Given an array nums of size n, return the majority element.
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ */
+
+const majorityElements = function (nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else {
+      map[nums[i]]++;
+    }
+  }
+
+  
+};
+
+const nums = [3, 2, 3];
